bab07b7116094334d4fd215e542e50c30e08821b
ethicalanp <ethicalanp66@gmail.com>
2025-12-18 20:49:51 +0530

    settings.py

--- ExpenseTracker/ExpenseTracker/settings.py
+++ ExpenseTracker/ExpenseTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['ExpenseTracker-env.eba-9tnhkgsu.ap-south-1.elasticbeanstalk.com']
 
